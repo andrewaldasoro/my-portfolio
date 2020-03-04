178cbbbf4e7ff6401b3fa5dbb300c87bba800197
Kev Aldasoro <andrewaldasoro@gmail.com>
2020-03-03 18:49:28 -0600

    Advance logo animation

--- src/components/react-logo/ReactLogo.tsx
+++ src/components/react-logo/ReactLogo.tsx
@@ -5,8 +5,13 @@ import './ReactLogo.scss';
 export default function ReactLogo() {
   return (
     <div>
-      <p>Developed with </p>
-      <img src={logo} className="logo" alt="logo" />
+      <div className="image-container">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+      <div className="text-container">
+        <p>Developed with </p>
+        <div className="fading-effect"></div>
+      </div>
     </div>
   );
 }
