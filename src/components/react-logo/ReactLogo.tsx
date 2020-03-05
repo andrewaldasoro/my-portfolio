f8734ec45f4341be93d470495fd6c6166dddd436
Kev Aldasoro <andrewaldasoro@gmail.com>
2020-03-05 15:23:26 -0600

    Logo animation checkpoint

--- src/components/react-logo/ReactLogo.tsx
+++ src/components/react-logo/ReactLogo.tsx
@@ -4,13 +4,13 @@ import './ReactLogo.scss';
 
 export default function ReactLogo() {
   return (
-    <div>
-      <div className="image-container">
-        <img src={logo} className="logo" alt="logo" />
-      </div>
+    <div className="logo-container">
       <div className="text-container">
         <p>Developed with </p>
         <div className="fading-effect"></div>
+      </div>
+      <div className="image-container">
+        <img src={logo} className="logo" alt="react logo" />
       </div>
     </div>
   );
