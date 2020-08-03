65d6b51a6d8021d83519bacdc0bac2c0f172c00c
Kev Aldasoro <andrewaldasoro@gmail.com>
2020-08-03 00:17:46 -0500

    F003. Add social networks information

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -8,9 +8,39 @@ import {
 } from "react-router-dom";
 import './Main.scss';
 import ReactLogo from '../react-logo'
+
+import githubLogo from '../../assets/logo-github.svg';
+import linkedinLogo from '../../assets/logo-linkedin.svg';
+import trelloLogo from '../../assets/logo-trello.svg';
+
 const pjson = require('../../../package.json');
 
 console.log(`Version: ${pjson.version}`);
+
+export class Emoji {
+  public render: () => JSX.Element;
+
+  private label: string | undefined;
+  private symbol: string;
+
+  constructor(symbol: string, label?: string) {
+    this.symbol = symbol;
+    this.label = label;
+
+    this.render = () =>
+      <span
+        className="emoji"
+        role="img"
+        title={this.label ? this.label : ""}
+        aria-label={this.label ? this.label : ""}
+        aria-hidden={this.label ? "false" : "true"}
+      >
+        {this.symbol}
+      </span>
+
+  }
+
+}
 
 // This site has 3 pages, all of which are rendered
 // dynamically in the browser (not server rendered).
@@ -46,31 +76,54 @@ function Body() {
 
 function Home() {
   const { t, i18n } = useTranslation();
-
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
   };
+
   return (
     <div className="App">
       <header className="header">
-        {t('title')}
+        {/* <h1>
+          {t('title')}
+        </h1> */}
+        <img src="bitmoji.png" alt="bitmoji"/> { /* TODO api for bitmoji profile */ }
+        <h1>Kev Andrew ALDASORO CHAVARRIA</h1>
+        <ul>
+          <li>
+            <Link to="/featured">{t('featured_projects')}</Link>
+          </li>
+          <li>
+            <Link to="/career">{t('career')}</Link>  { /* Could be studies */}
+          </li>
+          {/* <li>
+          <Link to="/contact" className="link">{t('contact')}</Link>  // TODO This will be on footer
+        </li> */}
+          {/* <li>
+          <Link to="/about" className="link">{t('about_me')}</Link> // TODO Home body
+        </li> */}
+        </ul>
       </header>
-      <ul>
-        <li>
-          <Link to="/about" className="link">{t('aboutme')}</Link>
-        </li>
-        <li>
-          <Link to="/featured" className="link">Featured Projects</Link>
-        </li>
-      </ul>
       <div className="body">
         <AboutMe />
+        { /* About me */}
       </div>
       <footer className="footer">
-        <div className="version">Version: {pjson.version}</div>
         <ReactLogo />
-        <button onClick={() => changeLanguage('en')}>en</button>
-        <button onClick={() => changeLanguage('es')}>es</button>
+        <address>
+          Email: <a href={"mailto:" + pjson.author.email}>{pjson.author.email}</a><br />
+          <a href="https://github.com/andrewaldasoro/cv" target="_blank" rel="noopener noreferrer">
+            <img src={githubLogo} alt="GitHub" title="GitHub" />
+          </a><br />
+          <a href="https://linkedin.com/in/andrewaldasoro/" target="_blank" rel="noopener noreferrer">
+            <img src={linkedinLogo} alt="LinkedIn" title="LinkedIn" />
+          </a><br/>
+          <a href="https://trello.com/b/2lJcsfa0" target="_blank" rel="noopener noreferrer">
+            <img src={trelloLogo} alt="Trello" title="Trello" />
+          </a>
+        </address>
+        <button onClick={() => changeLanguage('es')}>{new Emoji("🌮🇲🇽", t('spanish')).render()}</button>
+        <button onClick={() => changeLanguage('en')}>{new Emoji("🍁🇨🇦", t('english')).render()}</button>
+        <div className="version">{pjson.version}</div>
       </footer>
     </div>
   );
@@ -80,15 +133,19 @@ function AboutMe() {
   const { t } = useTranslation();
 
   return (
-    <h2>{t('aboutme')}</h2>
+    <div>
+      <h2>{t('aboutme')}</h2>
+      <div>
+      </div>
+    </div>
   );
 }
 
 function FeaturedProyects() {
+  const { t } = useTranslation();
+
   return (
-    <div>
-      <h2>Featured Proyects</h2>
-    </div>
+    <h2>{t('featured_projects')}</h2>
   );
 }
 
