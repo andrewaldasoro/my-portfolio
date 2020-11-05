8a0261845b74627a191c86875d5dd4fdc06e5440
Kev <andrewaldasoro@gmail.com>
2020-11-04 23:08:48 -0500

    Fix test browser roouter

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Header from "./Header";
+import { BrowserRouter } from "react-router-dom";
 
 describe("<Header />", () => {
   test("it should mount", () => {
-    render(<Header />);
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    );
 
     const header = screen.getByTestId("Header");
 
--- src/components/Navbar.test.tsx
+++ src/components/Navbar.test.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Navbar from "./Navbar";
+import { BrowserRouter } from "react-router-dom";
 
 describe("<Navbar />", () => {
   test("it should mount", () => {
-    render(<Navbar />);
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
 
     const navbar = screen.getByTestId("Navbar");
 
