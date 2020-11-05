1c992bc04b6e76cc2e7d1481eeacf17400ce4b2c
Kev <andrewaldasoro@gmail.com>
2020-11-04 22:51:46 -0500

    Comment test code

--- src/components/ProfileImage.lazy.tsx
+++ src/components/ProfileImage.lazy.tsx
@@ -1,11 +1,16 @@
 import React, { lazy, Suspense } from "react";
 import Loader from "./Loader";
 
-const LazyProfileImage = lazy(() => {
-  return new Promise<typeof import("./ProfileImage")>((resolve) => {
-    setTimeout(() => resolve(import("./ProfileImage")), 10000);
-  });
-});
+const LazyProfileImage = lazy(
+  () => import("./ProfileImage")
+  // {
+  //   return new Promise<typeof import("./ProfileImage")>((resolve) => {
+  //     setTimeout(() => resolve(import("./ProfileImage")), 10000);
+  //   });
+  // }
+
+  //To test Loader
+);
 
 const ProfileImage = (
   props: JSX.IntrinsicAttributes & { children?: React.ReactNode }
