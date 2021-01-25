5cb0bff222dddbb13838646de04fbe857707c563
Kev <andrewaldasoro@gmail.com>
2021-01-25 00:32:36 -0500

    :fire: New > Cursor mix-blend difference

--- src/components/EmojiLink.tsx
+++ src/components/EmojiLink.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./EmojiLink.scss";
 
 const EmojiLink = React.forwardRef<
   HTMLAnchorElement,
   { href: string; children: string; onClick: () => void }
->(({ href, children, onClick }, ref) => {
-  return (
-    <a className="EmojiLink" ref={ref} {...{ href, onClick }}>
-      {children}
-    </a>
-  );
-});
+>(({ href, children, onClick }, ref) => (
+  <a style={{ borderBottom: "none" }} ref={ref} {...{ href, onClick }}>
+    {children}
+  </a>
+));
 
 EmojiLink.displayName = "EmojiLink";
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,85 @@ import { render } from "react-dom";
 import "./index.scss";
 import "./i18n";
 import Main from "./components/Main";
+import gsap from "gsap";
 
 render(<Main />, document.getElementById("root"));
+
+gsap.set("#cursor", { force3D: true });
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  gsap.to("#cursor", {
+    x: x - 16,
+    y: y - 16,
+    ease: "power3",
+  });
+});
+
+document.body.addEventListener("mouseleave", () => {
+  gsap.to("#cursor", {
+    scale: 0,
+    duration: 0.1,
+    ease: "none",
+  });
+});
+
+document.body.addEventListener("mouseenter", () => {
+  if (
+    document.getElementById("cursor") &&
+    (document.getElementById("cursor") as HTMLDivElement).style.display === ""
+  ) {
+    (document.getElementById("cursor") as HTMLDivElement).style.display =
+      "block";
+  }
+
+  gsap.to("#cursor", {
+    scale: 1,
+    duration: 0.1,
+    ease: "none",
+  });
+});
+
+const textContainers = document.querySelectorAll('[data-cursor="text"]');
+
+textContainers.forEach((container) => {
+  container.addEventListener("mouseenter", () => {
+    gsap.to("#cursor", {
+      borderRadius: 0,
+      width: "10px",
+      scale: 0.6,
+      duration: 0.2,
+    });
+  });
+
+  container.addEventListener("mouseleave", () => {
+    gsap.to("#cursor", {
+      borderRadius: "50%",
+      width: "32px",
+      scale: 1,
+      duration: 0.2,
+    });
+  });
+});
+
+[...document.getElementsByTagName("a")].forEach((element) => {
+  element.setAttribute("data-cursor", "pointer");
+});
+const pointerContainers = document.querySelectorAll('[data-cursor="pointer"]');
+
+pointerContainers.forEach((container) => {
+  container.addEventListener("mouseenter", () => {
+    gsap.to("#cursor", {
+      scale: 0.5,
+      duration: 0.2,
+    });
+  });
+
+  container.addEventListener("mouseleave", () => {
+    gsap.to("#cursor", {
+      scale: 1,
+      duration: 0.2,
+    });
+  });
+});
